6196fba60134641bd27c721020c533a20ecc2d68
sakthi471 <sakthim61797@gmail.com>
2024-08-03 00:18:46 +0530

    ip test

---
+++ src/app/api/ip/route.js
@@ -0,0 +1,26 @@
+// pages/api/ip.js
+
+import { headers } from 'next/headers'
+
+
+function IP() {
+  const FALLBACK_IP_ADDRESS = '0.0.0.0'
+  const forwardedFor = headers().get('x-forwarded-for')
+ 
+  if (forwardedFor) {
+    return forwardedFor.split(',')[0] ?? FALLBACK_IP_ADDRESS
+  }
+ 
+  return headers().get('x-real-ip') ?? FALLBACK_IP_ADDRESS
+}
+
+export  const GET=async(req, res) =>{
+    try {
+        const ip = IP();
+      return Response.json({ip});
+    } catch (error) {
+       console.log(error.message);
+       
+    }
+  }
+  
