2048c574db1e635d524deb4e02d60ddbec12da11
sakthi471 <sakthim61797@gmail.com>
2024-09-16 02:14:35 +0530

    comment ip

--- src/app/api/ip/route.js
+++ src/app/api/ip/route.js
@@ -1,29 +1,29 @@
-// pages/api/ip.js
+// // pages/api/ip.js
 
-import { headers } from 'next/headers'
+// import { headers } from 'next/headers'
 
-import { unstable_noStore as noStore } from 'next/cache';
+// import { unstable_noStore as noStore } from 'next/cache';
 
 
 
-function IP() {
-  noStore()
-  const FALLBACK_IP_ADDRESS = '0.0.0.0'
-  const forwardedFor = headers().get('x-forwarded-for')
+// function IP() {
+//   noStore()
+//   const FALLBACK_IP_ADDRESS = '0.0.0.0'
+//   const forwardedFor = headers().get('x-forwarded-for')
 
-  if (forwardedFor) {
-    return forwardedFor.split(',')[0] ?? FALLBACK_IP_ADDRESS
-  }
+//   if (forwardedFor) {
+//     return forwardedFor.split(',')[0] ?? FALLBACK_IP_ADDRESS
+//   }
 
-  return headers().get('x-real-ip') ?? FALLBACK_IP_ADDRESS
-}
+//   return headers().get('x-real-ip') ?? FALLBACK_IP_ADDRESS
+// }
 
-export const GET = async (req, res) => {
-  try {
-    const ip = IP();
-    return Response.json({ ip });
-  } catch (error) {
-    console.log(error.message);
+// export const GET = async (req, res) => {
+//   try {
+//     const ip = IP();
+//     return Response.json({ ip });
+//   } catch (error) {
+//     console.log(error.message);
 
-  }
-}
+//   }
+// }
